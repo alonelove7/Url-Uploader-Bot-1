c06866719ee23ea469c115457fb1a894b6fe5ea0
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-28 10:15:20 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -31,7 +31,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 )
 async def start(bot, update):
     await update.reply_text(
-        text=Translation.START_TEXT.format(update.from_user.mention),
+        text=Scripted.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=Translation.START_BUTTONS
     )
