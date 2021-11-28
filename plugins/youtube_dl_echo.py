e05eaae15f81f41cc46c417fbc05fa7c9cb3a022
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-28 09:14:04 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -27,7 +27,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant, UserBannedInChannel
 from database.database import *
 from functions.display_progress import humanbytes
-from functions.uploadbot import DownLoadFile
+from functions.upload_bot import DownLoadFile
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
